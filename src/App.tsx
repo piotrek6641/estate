dcaf5cba4e435913fd731c65bba3f768d975d888
Piotr <piotrek.falba@gmail.com>
2023-10-19 21:00:47 +0200

    listing item prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import ListingItem from "./components/listings/ListingItem";
 
 function App() {
     return (
         <>
-            <div className='flex w-screen align-top h-screen'>
+            <div className='flex w-screen align-top h-screen flex-col'>
                 <Navbar/>
+                <div className="p-2 flex flex-col items-center">
+                    <ListingItem/>
+                </div>
+
             </div>
         </>
     );

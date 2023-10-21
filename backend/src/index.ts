b593f59752c090fc0ebce6c315542d5b30fa87fa
Piotr <piotrek.falba@gmail.com>
2023-10-21 19:32:38 +0200

    back-end initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import { createServer } from "http";
+
+const port = 3000;
+
+const server = createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello, World!\n");
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

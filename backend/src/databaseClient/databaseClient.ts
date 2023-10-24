c05406fe4844d81eb7c2abf94280db25030aceb4
Piotr <piotrek.falba@gmail.com>
2023-10-24 17:46:03 +0200

    add submodule/ clear dir structure

--- backend/src/databaseClient/databaseClient.ts
+++ backend/src/databaseClient/databaseClient.ts
@@ -1,18 +1,31 @@
 import { MongoClient } from "mongodb";
 
-export async function connectToMongoDB() {
-    const uri = process.env.MONGODB_URI as string;
+export class databaseClient {
+    private uri: string;
+    private client: MongoClient;
+    constructor() {
+        this.uri = process.env.MONGODB_URI as string;
+        this.client = new MongoClient(this.uri, { auth: {
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD }
+        });
+    }
+    async connectToMongoDB() {
+        await this.client.connect();
+        console.log("connected to db");
 
-    const client = new MongoClient(uri, { auth: { username: process.env.DB_USERNAME, password: process.env.DB_PASSWORD }  });
-    await client.connect();
-    console.log("connected to db");
+        const database = this.client.db("estates");
+        const collection = database.collection("houses");
 
-    const database = client.db("estates");
-    const collection = database.collection("houses");
+        const result = await collection.listIndexes();
 
-    const result = await collection.listIndexes();
+        console.log("Query Result:", result);
 
-    console.log("Query Result:", result);
-
-    await client.close();
+        await this.client.close();
+    }
+    async sendHandshake() {
+        await this.client.connect();
+        console.log("Handshake Successful");
+        await this.client.close();
+    }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
 import { createServer } from "http";
-import { connectToMongoDB } from "./databaseClient/databaseClient";
+import { databaseClient } from "./databaseClient/databaseClient";
 
 const port = process.env.PORT;
+const dbClient = new databaseClient();
+const server = createServer();
 
-const server = createServer(async (req, res) => {
+server.on("request", async (req, res) => {
+    console.log("received new server request", req.method, req.url);
     if (req.url === "/connect-to-db" && req.method === "GET") {
         try {
-            await connectToMongoDB();
+            await dbClient.sendHandshake();
             res.writeHead(200, { "content-type": "text/plain" });
             res.end("Successfully connected to db ;)");
         } catch {
@@ -17,9 +20,7 @@ const server = createServer(async (req, res) => {
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("Hello, World!\n");
     }
-
 });
-
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });

e68ec51877f4997a88ca700da322e6730732bcb5
Piotr <piotrek.falba@gmail.com>
2023-10-26 19:33:58 +0200

    Change ApiGateway to class

--- backend/ApiGateway/src/index.ts
+++ backend/ApiGateway/src/index.ts
@@ -2,28 +2,51 @@ import http = require("http");
 import { createProxyServer } from "http-proxy";
 import { Logger } from "logger";
 
-const proxy = createProxyServer();
-const logger = new Logger("ApiGateway");
+export class ApiGateway {
+    proxy = createProxyServer();
+    logger = new Logger("ApiGateway");
+    port = 3000;
+    gateway;
 
-const gateway = http.createServer((req, res) => {
-    const routes: { [key: string]: string } = {
-        "service1": "http://localhost:11000"
-    };
-    if (!req.url) return;
-    const path = req.url.split("/")[1];
+    constructor() {
+        this.gateway = this.createServer();
 
-    if (routes[path]) {
-        const target = routes[path];
-        logger.info(`forwarding request to: ${target} (${path})`);
-        proxy.web(req, res, { target });
-        logger.info(`got response ${res.statusCode} from ${target}`);
-    } else {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.end("Route not found");
+        this.gateway.listen(this.port, () => {
+            this.logger.info(`Complex Gateway API listening on port ${this.port}`);
+        });
+
     }
-});
 
-const port = 3000;
-gateway.listen(port, () => {
-    logger.info(`Complex Gateway API listening on port ${port}`);
-});
+    createServer() {
+        return http.createServer((req, res) => {
+            const routes: { [key: string]: string } = {
+                "service1": "http://localhost:11000"
+            };
+            if (!req.url) return;
+            const path = req.url.split("/")[1];
+
+            if (routes[path]) {
+                const target = routes[path];
+
+                this.logger.info(`forwarding request to: ${target} (${path})`);
+                try {
+                    this.proxy.web(req, res, { target }, (err) => {
+                        if (err) {
+                            this.logger.error(`Error when forwarding request to ${target}: ${err.message}`);
+                            res.writeHead(500, { "Content-Type": "text/plain" });
+                            res.end("Proxy Error");
+                        } else {
+                            this.logger.info(`got response ${res.statusCode} from ${target}`);
+                        }
+                    });
+                } catch(e) {
+                    this.logger.warning(e as string);
+                }
+            } else {
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                res.end("Route not found");
+            }
+        });
+    }
+
+}

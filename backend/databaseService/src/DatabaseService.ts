376a1d4b886239aecdae3248216155372dc45510
Piotr <piotrek.falba@gmail.com>
2023-10-27 16:44:33 +0200

    added service that start docker container with db

---
+++ backend/databaseService/src/DatabaseService.ts
@@ -0,0 +1,56 @@
+// import mysql = require("mysql2");
+import { DockerClient } from "./docker";
+import { Logger } from "logger";
+import { MongoClient } from "mongodb";
+
+export class DatabaseService{
+    logger;
+    dockerClient: DockerClient;
+    constructor() {
+        this.dockerClient = new DockerClient();
+        this.logger = new Logger("DatabaseService");
+        this.startDB();
+    }
+    async startDB() {
+        try {
+            await this.pullImage();
+            await this.startImage();
+            await this.sendHandshake();
+        } catch (error) {
+            this.logger.error("Error starting the database:" + error);
+        }
+    }
+    async pullImage() {
+        return this.dockerClient.pullDBImage()
+            .then((data) => {
+                console.log(data);
+            }).catch((e) => {
+                this.logger.error("failed to pull db image");
+                throw new Error(e);
+            });
+    }
+    async startImage() {
+        return this.dockerClient.createAndStartDockerContainer()
+            .then(async () => {
+                return await this.dockerClient.waitForContainerToBoot();
+            });
+
+    }
+    async sendHandshake() {
+        const connectionString = "mongodb://localhost:27017/mydatabase";
+
+        try {
+            const client = new MongoClient(connectionString, {
+            });
+            await client.connect();
+
+            this.logger.info("Connected to MongoDB");
+
+            client.close();
+            this.logger.info("Connection closed");
+        } catch (error) {
+            this.logger.error("Error connecting to MongoDB:" + error);
+        }
+    }
+
+}
